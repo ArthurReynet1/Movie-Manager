0b7313f6a46063d08b05dfca176e88c62a2fb7d3
Arthur Reynet <arthur.reynet@viacesi.fr>
2026-01-09 09:27:19 +0100

    feat : added navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import MovieList from "./components/MovieList";
 import Wishlist from "./components/Wishlist";
 import MovieDetails from "./components/MovieDetails";
 import { PATHS } from "./routes";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path={PATHS.HOME} element={<MovieList />} />
         <Route path={PATHS.WISHLIST} element={<Wishlist />} />
